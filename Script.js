551b152c7dad7e9deb84bd37f716177f721c8e08
Beata009 <bkolodziejszymanska@gmail.com>
2024-06-05 22:17:45 +0200

    zadanie

---
+++ Script.js
@@ -0,0 +1,13 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+console.log("Zawartość tablicy fruits:", fruits);
+
+const numbers = [2, 4, 6, 8, 1, 7];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 5) {
+        console.log(numbers[i] + " jest większe niż 5");
+    }
+}
+
+
